684f65865b75e645debb9164bf65001ca24c4b7e
ReRuol <970960818ReRuol@users.noreply.github.com>
2020-06-02 12:32:50 +0800

    更新整个项目

--- js/flexible.js
+++ js/flexible.js
@@ -1,6 +1,6 @@
 //designWidth:设计稿的实际宽度值，需要根据实际设置
 //maxWidth:制作稿的最大宽度值，需要根据实际设置
-//这段js的最后面有两个参数记得要设置，一个为设计稿实际宽度，一个为制作稿最大宽度，例如设计稿为750，最大宽度为750，则为(750,750)
+//这段js的最后面有两个参数记得要设置，一个为设计稿实际宽度，一个为制作稿最大宽度，例如设计稿实际宽度为750，制作稿最大宽度为750，则为(750,750)
 ;(function(designWidth, maxWidth) {
     var doc = document,
         win = window,
@@ -12,7 +12,7 @@
         var width = docEl.getBoundingClientRect().width;
         maxWidth = maxWidth || 540;
         width>maxWidth && (width=maxWidth);
-        var rem = width * 100 / designWidth;
+        var rem = width * 20 / designWidth;
         remStyle.innerHTML = 'html{font-size:' + rem + 'px;}';
     }
 
@@ -24,7 +24,7 @@
         doc.write(wrap.innerHTML);
         wrap = null;
     }
-    //要等 wiewport 设置好后才能执行 refreshRem，不然 refreshRem 会执行2次；
+    //要等 viewport 设置好后才能执行 refreshRem，不然 refreshRem 会执行2次；
     refreshRem();
 
     win.addEventListener("resize", function() {
